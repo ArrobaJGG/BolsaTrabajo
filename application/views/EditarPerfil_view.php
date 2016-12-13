6eefe2aea8e40d0b6071d86b2bd90f7984f72d1e
GorkaL <landerskate@hotmail.com>
2016-12-13 13:49:33 +0100

    footer y editar perfil cambiado

--- application/views/EditarPerfil_view.php
+++ application/views/EditarPerfil_view.php
@@ -7,39 +7,39 @@ $this->load->helper('form');
 	echo form_open('EditarPerfil_controller');
 	?>
 
-Nombre: <input type="text" name="nombre" value="">
-Apellido: <input type="text" name="apellido" value="">
-Telefono: <input type="text" name="telefono" value="">
-DNI: <input type="text" name="" value="">
-Fecha Nacimiento: <input type="date" name="" value="">
-Codigo Postal: <input type="text" name="" value="" placeholder="38587">
-Descripcion: <textarea name="Descripcion"></textarea>
+Nombre: <input type="text" name="nombre" value="" placeholder="nombre"></br>
+Apellido: <input type="text" name="apellido" value="" placeholder="apellido"></br>
+Telefono: <input type="text" name="telefono" value="" placeholder="telefono"></br>
+DNI: <input type="text" name="" value="" placeholder="dni"></br>
+Fecha Nacimiento: <input type="date" name="" value="" placeholder="fecha nacimiento"></br>
+Codigo Postal: <input type="text" name="" value="" placeholder="38587"></br>
+Descripcion: <textarea name="Descripcion" placeholder="introduce aqui..."></textarea></br>
 Familia: <select name="familia">
              	<option value=informatica>Informatica</option>
 	            <option value="quimica">Quimica</option>
 	            <option value="administracion">Administracion</option>
 	            <option value="electronica">Electronica</option>
-        </select>
+        </select></br>
 Idiomas: <select name="Idiomas">
 	<option value="Castellano">Castellano</option>
 	<option value="Euskera">Euskera</option>
 	<option value="Ingles">Ingles</option>
 	<option value="Frances">Frances</option>
 	<option value="Aleman">Aleman</option>
-</select>
+</select></br>
 Nivel: <select name="nivel">
 	<option value="Alto">Alto</option>
 	<option value="Medio">Medio</option>
 	<option value="Bajo">Bajo</option>
-</select>
+</select></br>
 Curso: <select name="Curso">
 	<option value="Desarrollo_web">Desarrollo Web</option>
 	<option value="Finanzas">Finanzas</option>
 	<option value="Quimica">Quimica</option>
 	<option value="Electronica">Electronica</option>
-	</select>
-Año Inicio: <input type="number" name="ano_inicio" value="" min="1960" max="2016">
-Año Fin: <input type="number" name="ano_fin" value="" min="1980" max="2017">
-Expiriencia: <textarea name="Expiriencia"></textarea>
-Foto: <input type="file" name="" value="">
+	</select></br>
+Año Inicio: <input type="number" name="ano_inicio" value="" min="1960" max="2016"></br>
+Año Fin: <input type="number" name="ano_fin" value="" min="1980" max="2017"></br>
+Expiriencia: <textarea name="Expiriencia" placeholder="experiencia..."></textarea></br>
+Foto: <input type="file" name="" value=""></br>
 </form>	
--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,2 +1,13 @@
+
+
+
+
+
+<footer>
+  <p>© CIFP Txurdinaga LHII</p>
+  <p>Teléfono: 944125712
+Fax: 944731359
+idazkaria@fpTXurdinaga.com</p>
+</footer>
 </body>
 </html>
