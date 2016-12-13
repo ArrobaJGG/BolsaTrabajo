651634182de7951a80b7bbb2035d39993b697781
GorkaG <gargoka@hotmail.com>
2016-12-13 14:10:59 +0100

    devuelto el login_controller

---
+++ application/controllers/Login_Controller.php
@@ -0,0 +1,18 @@
+<?php
+class Login_Controller extends CI_Controller{
+//	
+public function __construct(){
+    parent::__construct();
+    $this->load->model('login_model');
+	//para poder ir de un controlador a otro facilmente
+    $this->load->helper('url_helper');
+}
+//	
+public function index(){
+	$this->load->view("includes/header");
+	$this->load->view("login_view");
+	$this->load->view("includes/footer");
+	$this->load->view("Editarempresa_view");
+}
+}
+?>
