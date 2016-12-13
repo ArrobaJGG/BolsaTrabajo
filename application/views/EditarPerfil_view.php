3e651c5dfd9679cb94b470a32ed9ed859b435062
ik_2dw3 <ik_2dw3@H015110.NET>
2016-12-13 09:46:33 +0100

    modificaciones en editarperfiles.php

--- application/views/EditarPerfil_view.php
+++ application/views/EditarPerfil_view.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Editar Perfil</title>
-	<link rel="stylesheet" href="">
-</head>
+
 <body>
 Nombre: <input type="text" name="nombre" value="">
 Apellido: <input type="text" name="apellido" value="">
