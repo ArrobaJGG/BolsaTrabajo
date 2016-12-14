b75c1645822eb104257f40d864918338614b691a
JuanLuisVegaAlonso <juanlu.vega993@gmail.com>
2016-12-14 10:47:08 +0100

    getRol
    Login_model->get_rol

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -14,7 +14,14 @@ class Login_model extends CI_Model{
 	}	
 	public function get_contrasena($correo){
 		$slq = "SELECT contrasena FROM login WHERE correo='$correo'";
-		return $this->db->query($sql);
+		$query = $this->db->query($sql);
+		return $query->row();
+	}
+	public function get_rol($correo){
+		$slq = "SELECT rol FROM login WHERE correo='$correo'";
+		$query = $this->db->query($sql);
+		return $query->row();
+		
 	}
 	public function actualizar_ultimo_login($id_login,$fecha){
 		$sql = "UPDATE login SET ultimo_login = '$fecha' where id_login='$id_login'";
