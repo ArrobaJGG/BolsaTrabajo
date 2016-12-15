55ed24d8adbb7ef81a105d760a452b16c302f78e
JuanLuisVegaAlonso <juanlu.vega993@gmail.com>
2016-12-15 13:55:03 +0000

    dominio

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://hostpiberry.ddns.net/BolsaTrabajo/';
+$config['base_url'] = './';
 
 /*
 |--------------------------------------------------------------------------
