bf60897f1a6902104bbb4ace7d70458eeb39d6dd
JuanLuisVegaAlonso <juanlu.vega993@gmail.com>
2016-12-19 14:23:10 +0100

    Pagina registro con validacion en cliente completada

--- application/views/registro_view.php
+++ application/views/registro_view.php
@@ -6,19 +6,28 @@ $this->load->helper('form');
 	<?php
 	echo validation_errors(); 
 	?>
-	<form ng-submit="validar($event)"ng-controller = "formulario" action="./Registro_controller" method="post" accept-charset="utf-8">
+	<form id="formulario" ng-submit="validar($event)"name="formulario" ng-controller = "formulario_ctrl" action="./Registro_controller" novalidate="" method="post" accept-charset="utf-8">
 		<table id="registro" title="Registro">
 			<tr>
 				<td>Usuario: </td>
-				<td><input ng-model="usuario.correo" type="text" name="usuario" /></td>
+				<td>
+					<input ng-model="usuario.correo" pattern=".{3,45}"  type="email" name="usuario" id="usuario" required/>
+					<span ng-show="!formulario.usuario.$valid">Usuario incorrecto</span>
+				</td>
 			</tr>
 			<tr>
 				<td>Contraseña</td>
-				<td><input class = "confirmar" ng-model="usuario.contrasena1" type="password" name="contrasena" /></td>
+				<td>
+					<input class = "confirmar" ng-model="usuario.contrasena1"  pattern=".{3,45}"  type="password" id="contrasena" name="contrasena" required />
+					<span ng-show="!formulario.contrasena.$valid">Contraseña incorrecta</span>
+				</td>
 			</tr>
 			<tr>
 				<td>Confirmar</td>
-				<td><input class="confirmar" ng-model="usuario.contrasena2" type="password" name="contrasenaC" /></td>
+				<td>
+					<input class="confirmar" ng-model="usuario.contrasena2" pattern=".{3,45}"  type="password" id="contrasenaC" name="contrasenaC" required/>
+					<span ng-show="!contrasenas_iguales()">Contraseñas no iguales</span>
+				</td>
 			</tr>
 		</table>
 		<input type="submit" id="Enviar" name="Enviar" value="Enviar" />
--- assets/js/registro_controller.js
+++ assets/js/registro_controller.js
@@ -1,12 +1,23 @@
 var app = angular.module("my-app",[]);
-app.controller("formulario",['$scope',function($scope){
-	$scope.validar = function($event){
-		var contrasena1 = $scope.usuario.contrasena1;
-		var contrasena2 = $scope.usuario.contrasena2;
-		alert("hooolalaaa");
-		if(contrasena1!=contrasena2){
-			$event.preventDefault();
-			alert("adios");
+app.controller("formulario_ctrl",['$scope','$document',function($scope,$document){
+	var error = false;
+	$scope.contrasenas_iguales = function(){
+		var iguales = false;
+		if($scope.formulario.contrasena.$valid && $scope.formulario.contrasenaC.$valid){
+			var contrasena1 = $scope.usuario.contrasena1;
+			var contrasena2 = $scope.usuario.contrasena2;
+			if(contrasena1 == contrasena2){
+				return true;
+			}
 		}
+		return false;
+	};
+	$scope.validar = function($event){
+		error = true;
+		if($scope.formulario.usuario.$valid&&$scope.formulario.contrasena.$valid&&$scope.contrasenas_iguales()){
+			error = false;
+		}
+		
+		if(error) $event.preventDefault();
 	} ;
 }]);
