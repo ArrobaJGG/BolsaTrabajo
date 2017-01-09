4a1528fa3d2a25891e7ad195052aed98eb87d936
GorkaG <gargoka@hotmail.com>
2017-01-09 09:04:25 +0100

    cosas utiles

--- application/views/editar_perfil_view.php
+++ application/views/editar_perfil_view.php
@@ -1,16 +1,11 @@
 <?php 
 $this->load->helper('form');
  ?>
-
-	
-	<?php 
-	echo form_open('EditarPerfil_controller');
-	?>
   <div ng-controller="UserController">
-  	 <form name="userForm" ng-submit="submit($event)" action="./Editarperfil_controller" method="post" novalidate>
-		Nombre: <input name="name" type="text" ng-model="user.name"  required />
-		<span class="messages" ng-show="userForm.$submitted || userForm.name.$touched">
-		<span ng-show="userForm.name.$error.required">El campo es obligatorio.</span>
+  	 <form name="userForm" ng-submit="submit($event)" action="./Editar_perfil_controller" method="post" novalidate>
+		Nombre: <input name="nombre" type="text" ng-model="user.nombre"  required />
+		<span class="messages" ng-show="userForm.$submitted || userForm.nombre.$touched">
+		<span ng-show="userForm.nombre.$error.required">El campo es obligatorio.</span>
 		</span></br>
 		
 		Apellido: <input type="text" name="apellido" ng-model="user.apellido" placeholder="apellido" required>
@@ -25,7 +20,7 @@ $this->load->helper('form');
         <span ng-show="userForm.telefono.$error.max">el telefono no puede exceder de 13.</span>
   		</span></br>
 		
-		DNI: <input type="text" name="DNI" ng-model="user.apellido" placeholder="dni" required>
+		DNI: <input type="text" name="DNI" ng-model="user.dni" placeholder="dni" required>
 		<span class="messages" ng-show="userForm.$submitted || userForm.dni.$touched">
         <span ng-show="userForm.dni.$error.required">El campo es obligatorio.</span>
         </span></br>
@@ -74,13 +69,13 @@ $this->load->helper('form');
 		Año Inicio: <input type="number" name="ano_inicio" ng-model="ano_incio"  min="1960" >
 		<span class="messages" ng-show="userForm.$submitted || userForm.ano_inicio.$touched">
         <span ng-show="userForm.ano_inicio.$error.required">El campo es obligatorio.</span>
-        <span ng-show="userForm.ano_inicio.$error.max">el año de inicio no puede ser inferior de 1960.</span>
+        <span ng-show="userForm.ano_inicio.$error.min">el año de inicio no puede ser inferior de 1960.</span>
         </span></br>
    
 		Año Fin: <input type="number" name="ano_fin" ng-model="ano_fin" min="1980" >
 		<span class="messages" ng-show="userForm.$submitted || userForm.ano_fin.$touched">
         <span ng-show="userForm.ano_fin.$error.required">El campo es obligatorio.</span>
-        <span ng-show="userForm.ano_fin.$error.max">el año de finalizacion no puede ser inferior de 1980.</span>
+        <span ng-show="userForm.ano_fin.$error.min">el año de finalizacion no puede ser inferior de 1980.</span>
         </span></br>
 		
 		Experiencia: <textarea name="experiencia" ng-model="experiencia" ng-maxlength="1000" placeholder="experiencia..."></textarea>
@@ -94,5 +89,7 @@ $this->load->helper('form');
         <span ng-show="userForm.foto.$error.required">El campo es obligatorio.</span>
         </span></br>
 		
+		<p><?php if(isset($mensaje)) echo $mensaje ?></p>
+        <?php echo validation_errors(); ?><!--mostrar los errores de validación-->
 		<input type="submit" value="Enviar"/>
 		</form>	
