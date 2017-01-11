b7f336d4a7473edbdb085b290f5becb1db9a5da5
JuanLuisVegaAlonso <juanlu.vega993@gmail.com>
2017-01-11 10:28:48 +0100

    Familia model

---
+++ application/models/Familia_laboral_model.php
@@ -0,0 +1,14 @@
+<?php 
+class alumno_model extends CI_Model{
+	public function __construct()
+        {
+                $this->load->database();
+        }
+	public function familia()
+	{
+				$query=$this->db->query("SELECT * FROM familia_laboral");
+				$row = $query->result_array();
+				$familia = $row ? $row : false;
+				return $familia;
+	}
+}
