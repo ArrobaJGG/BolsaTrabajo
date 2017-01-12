25816dc216cbded07d38e2bb2b00494b81bba14e
JuanLuisVegaAlonso <juanlu.vega993@gmail.com>
2017-01-12 10:22:30 +0100

    alumno model

--- application/models/alumno_model.php
+++ application/models/alumno_model.php
@@ -1,15 +1,32 @@
 <?php 
-class alumno_model extends CI_Model{
+class Alumno_model extends CI_Model{
 	public function __construct()
         {
                 $this->load->database();
         }
-	public function familia()
-	{
-				$query=$this->db->query("SELECT * FROM familia_laboral");
-				$row = $query->result_array();
-				$familia = $row ? $row : false;
-				return $familia;
+	public function get_nombre($id_login){
+		$sql = "SELECT nombre FROM alumno WHERE id_login = '$id_login'";
+		$query = $this->db->query($sql);
+		$row = $query->row();
+		$devolver = isset($row) ? $row->nombre : false;
+		return $devolver;
+	}
+	public function actualizar_alumno($columnas,$datos){
+		/*
+		 * Cambiar insert por update y alguna cosa mas para poder actualizar solo los campos que se envian
+		 */
+		$sql = "INSERT INTO alumno (";
+		foreach ($columnas as $columna) {
+			$sql += $columna.',';
+		}
+		trim($sql,',');
+		$sql += ') VALUES (';
+		foreach ($datos as $dato) {
+			$sql += $dato.',';
+		}
+		trim($sql,',');
+		$sql += ')';
+		return $this->db->query($sql);
 	}
 }
 ?>
