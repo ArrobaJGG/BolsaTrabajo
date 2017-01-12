a3220a566b270d1bb8505f8b803df42b106722d6
JuanLuisVegaAlonso <juanlu.vega993@gmail.com>
2017-01-12 10:28:54 +0100

    cargar partes

---
+++ application/controllers/api/Cargar_partes.php
@@ -0,0 +1,11 @@
+<?php
+class Cargar_partes extends CI_Controller{
+	
+public function __construct(){
+	parent::__construct();
+	$this->load->helper('url_helper');
+}
+public function cargar($pagina){
+	$this->load->view("partes/$pagina");
+}
+}
