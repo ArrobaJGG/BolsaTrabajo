6fab0d4d07318403bf78a295924b25f29778266e
GorkaG <gargoka@hotmail.com>
2017-01-26 14:54:13 +0100

    fix

--- application/controllers/Editar_perfil_controller.php
+++ application/controllers/Editar_perfil_controller.php
@@ -1,29 +1,30 @@
 <?php
 class Editar_perfil_controller extends CI_Controller{
 	
-public function __construct(){
-parent::__construct();
-$this->load->model('familia_laboral_model');
-$this->load->model('idioma_model');
-$this->load->model('alumno_model');
-$this->load->model('curso_model');
+	public function __construct(){
+		parent::__construct();
+		$this->load->model('familia_laboral_model');
+		$this->load->model('idioma_model');
+		$this->load->model('alumno_model');
+		$this->load->model('curso_model');
 //para poder ir de un controlador a otro facilmente
-$this->load->helper(array('form','url'));
-$this->load->library('form_validation'); 
-$this->load->library('session');
+		$this->load->helper(array('form','url'));
+		$this->load->library('form_validation'); 
+		$this->load->library('session');
+		$this->load->library('upload');
+		$this->load->library('image_lib');
 
+	}
 
-}
-
-		
-public function index(){
 	
-	if ($this->session->userdata('rol')=='alumno') {		
-		$id_login = $this->session->userdata['id_login'];
-		$rol = $this->session->userdata['rol'];
+	public function index(){
 		
-	if ($this->input->post('Enviar')){
-		
+		if ($this->session->userdata('rol')=='alumno') {		
+			$id_login = $this->session->userdata['id_login'];
+			$rol = $this->session->userdata['rol'];
+			
+			if ($this->input->post('Enviar')){
+				
 				$this->form_validation->set_rules('nombre', 'nombre ', 'required|callback_letras');
 				$this->form_validation->set_rules('apellido','apellido','callback_letras');
 				$this->form_validation->set_rules('telefono','telefono','integer');
@@ -41,115 +42,133 @@ public function index(){
 				$this->form_validation->set_message('min_length','El campo %s debe tener mas de 3 caracteres');
 				$this->form_validation->set_message('max_length','El campo %s debe tener como maximo 1000 caracter');
 				
-			    if($this->form_validation->run() ==false){
+				if($this->form_validation->run() ==false){
 			    	 //$datos["mensaje"] = "Validacion incorrecta";
 					
 				}else{
-				$nombre = $this->input->post('nombre');
-				$apellidos = $this->input->post('apellidos');
-				$telefono = $this->input->post('telefono');
-				$dni = $this->input->post('dni');
-				$fecha_nacimiento = $this->input->post('fecha_nacimiento');
-				$codigo_postal = $this->input->post('codigo_postal');
-				$resumen = $this->input->post('resumen');
-				$familia = $this->input->post('familia');
-				$idioma = $this->input->post('idioma');
-				$nivelleido = $this->input->post('nivelleido');
-				$nivelescrito = $this->input->post('nivelescrito');
-				$nivelhablado = $this->input->post('nivelhablado');
-				$titulado = $this->input->post('titulado');
-				$curso = $this->input->post('curso');
-				$fecha_inicio = $this->input->post('fecha_inicio');
-				$fecha_final = $this->input->post('fecha_final');
-				$experiencia = $this->input->post('experiencia');
-				$logo = $this->input->post('logo');
-					 $parametros_alumno = array( "nombre" => $nombre, 
-					   						"dni" => $dni,
-					   						"apellidos" => $apellidos ,
-					   						"telefono" => $telefono,
-					   						"fecha_nacimiento" => $fecha_nacimiento,
-					   						"id_login" => $id_login,
-					   						"codigo_postal" => $codigo_postal,
-					   						"resumen" => $resumen,
-					   						"experiencia" => $experiencia,
-					   						"logo" => $logo);
-					  $parametros_alumno_curso= array("fecha_inicio" => $fecha_inicio,
-					  								  "fecha_final" => $fecha_final,
-													  "id_login" => $id_login
-													  );
-					  $parametros_nivel= array("nivelleido" => $nivelleido,
-					  						   "nivelescrito" => $nivelescrito,
-					  						   "nivelhablado" => $nivelhablado
-					  							);
-					  $parametros_familia= array( "familia" => $familia,
-					  							  "id_login" => $id_login);
-					    $mi_archivo = 'logo';
-				        $config['upload_path'] = './img/';
-						//$config['default'] = './img/pordefecto.jpg/';
-						$config['overwrite'] = TRUE;
-						$config['upload_path'] = FCPATH . './img/';
-						$config['file_name'] = $id_login;
-						$config['allowed_types'] = 'jpg';
-						//$config['max_size'] = "50000";//kb
-						//$config['max_width'] = "2000"; // kb
-						//$config['max_height'] = "2000";// kb
-								
-						$this->load->library('upload', $config);
-						$this->upload->initialize($config);
-								        
-						if (!$this->upload->do_upload($mi_archivo)) {
-								//*** ocurrio un error
-								/*$data['uploadError'] = $this->upload->display_errors();
-								echo $this->upload->display_errors();
-								return;*/
-						}
+					$nombre = $this->input->post('nombre');
+					$apellidos = $this->input->post('apellidos');
+					$telefono = $this->input->post('telefono');
+					$dni = $this->input->post('dni');
+					$fecha_nacimiento = $this->input->post('fecha_nacimiento');
+					$codigo_postal = $this->input->post('codigo_postal');
+					$resumen = $this->input->post('resumen');
+					$familia = $this->input->post('familia');
+					$idioma = $this->input->post('idioma');
+					$nivelleido = $this->input->post('nivelleido');
+					$nivelescrito = $this->input->post('nivelescrito');
+					$nivelhablado = $this->input->post('nivelhablado');
+					$titulado = $this->input->post('titulado');
+					$curso = $this->input->post('curso');
+					$fecha_inicio = $this->input->post('fecha_inicio');
+					$fecha_final = $this->input->post('fecha_final');
+					$experiencia = $this->input->post('experiencia');
+					$logo = $this->input->post('logo');
+					$parametros_alumno = array( "nombre" => $nombre, 
+						"dni" => $dni,
+						"apellidos" => $apellidos ,
+						"telefono" => $telefono,
+						"fecha_nacimiento" => $fecha_nacimiento,
+						"id_login" => $id_login,
+						"codigo_postal" => $codigo_postal,
+						"resumen" => $resumen,
+						"experiencia" => $experiencia,
+						"logo" => $logo);
+					$parametros_alumno_curso= array("fecha_inicio" => $fecha_inicio,
+						"fecha_final" => $fecha_final,
+						"id_login" => $id_login
+						);
+					$parametros_nivel= array("nivelleido" => $nivelleido,
+						"nivelescrito" => $nivelescrito,
+						"nivelhablado" => $nivelhablado
+						);
+					$parametros_familia= array( "familia" => $familia,
+						"id_login" => $id_login);
+					$my_image='logo';
+					$config['upload_path'] = './img';
+					$config['allowed_types'] = 'gif|jpg|png|jpeg';
+					$config['max_size'] = '2048';
+					$config['encrypt_name'] = TRUE;
+					$this->load->library('upload', $config);
+					if (!$this->upload->do_upload("my_image")) {
+						echo $this->upload->display_errors(); exit();
+					} else {     
+						$data = array('upload_data' => $this->upload->data());
+						$img_full_path = $config['upload_path'] . $data['upload_data']['file_name'];
 						
-														
-						$data['uploadSuccess'] = $this->upload->data();
-   			 
-					   //$actualizar_alumno = $this->alumno_model->actualizar($parametros,$id_login);
-					  
-					   	$actualizar_alumno = $this->alumno_model->actualizar_alumno($parametros_alumno,$id_login);
-						$actualizar_alumno = $this->alumno_model->actualizar_alumno_curso($parametros_alumno_curso,$id_login);
-					   //$actualizar_alumno=$this->alumno_model->actualizar($nombre,$apellidos,$telefono,$dni,$fecha,$codigo_postal,$descripcion,$experiencia);
-					   	$datos["mensaje"] = "Validacion correcta";
-					   
-					
-				}
+     // REDIMENSIONAMOS
+						$config['image_library'] = 'gd2';
+						$config['source_image'] = $img_full_path;
+						$config['maintain_ratio'] = TRUE;
+						$config['width'] = 275;
+						$config['height'] = 250;
+						$config['new_image'] = './img/imgr/'. $data['upload_data']['file_name'];
+						$img_redim1 = $config['new_image'];
+						$this->load->library('image_lib', $config);
+						if (!$this->image_lib->resize()) {
+							@unlink($img_full_path);
+							echo $this->image_lib->display_errors(); exit();
+						}
+						$this->image_lib->clear();
 
-			    
+     // REDIMENSIONAMOS DE NUEVO
+						$config['image_library'] = 'gd2';
+						$config['source_image'] = $img_full_path;
+						$config['maintain_ratio'] = TRUE;
+						$config['width'] = 75;
+						$config['height'] = 50;
+						$config['new_image'] = './img/imgr/'. $data['upload_data']['file_name'];
+     $this->image_lib->initialize($config); /// <<- IMPORTANTE
+     if (!$this->image_lib->resize()) {
+     	@unlink($img_full_path);
+     	@unlink($img_redim1);
+     	echo $this->image_lib->display_errors(); exit();
+     }
+ }
+ 
+					   //$actualizar_alumno = $this->alumno_model->actualizar($parametros,$id_login);
+ 
+ $actualizar_alumno = $this->alumno_model->actualizar_alumno($parametros_alumno,$id_login);
+ $actualizar_alumno = $this->alumno_model->actualizar_alumno_curso($parametros_alumno_curso,$id_login);
+					   //$actualizar_alumno=$this->alumno_model->actualizar($nombre,$apellidos,$telefono,$dni,$fecha,$codigo_postal,$descripcion,$experiencia);
+ $datos["mensaje"] = "Validacion correcta";
+ 
+ 
+}
+
+
 
 }
-				$datos_alumnos = $this->alumno_model->id_login($id_login);
-     			$datos_alumnos['cursos'] = $this->alumno_model->alumno_curso($id_login);
-     			$data['libreria']=array();
-     			$data['titulo'] = "Editar Perfil";
-     			$data["javascript"]="assets/js/editar_perfil.js";
-     			$data['idiomas']=$this->idioma_model->idioma();
-     			$data['niveles']=$this->idioma_model->nivelleido();
-     			$data['niveleshablados']=$this->idioma_model->nivelhablado();
-     			$data['nivelesescritos']=$this->idioma_model->nivelescrito();
-     			$data['familias']=$this->familia_laboral_model->familia();
+$datos_alumnos = $this->alumno_model->id_login($id_login);
+$datos_alumnos['cursos'] = $this->alumno_model->alumno_curso($id_login);
+$data['libreria']=array();
+$data['titulo'] = "Editar Perfil";
+$data["javascript"]="assets/js/editar_perfil.js";
+$data['idiomas']=$this->idioma_model->idioma();
+$data['niveles']=$this->idioma_model->nivelleido();
+$data['niveleshablados']=$this->idioma_model->nivelhablado();
+$data['nivelesescritos']=$this->idioma_model->nivelescrito();
+$data['familias']=$this->familia_laboral_model->familia();
 				//$data['cursos']=$this->alumno_model->alumno_curso($id_login);
-     			
-     			$this->load->view("includes/header",$data);
-     			$this->load->view("Editar_perfil_view", $datos_alumnos);
-     			$this->load->view("includes/footer",$data );	
+
+$this->load->view("includes/header",$data);
+$this->load->view("Editar_perfil_view", $datos_alumnos);
+$this->load->view("includes/footer",$data );	
 }
 else{
 	redirect('login_controller');
 }
 }
-     	public function letras($cadena){
-     		if (!preg_match( '/[0-9]+$/i', $cadena ))
-     		{
-     			if(preg_match( '/^[a-z ,.]*$/i', $cadena ))
-     				return TRUE;
-     		}
-     		else {
-     			$this->form_validation->set_message('letras', 'el campo %s tiene que tener solo letras');
-     			return FALSE;
-     		}
-     	}
-     	}
+public function letras($cadena){
+	if (!preg_match( '/[0-9]+$/i', $cadena ))
+	{
+		if(preg_match( '/^[a-z ,.]*$/i', $cadena ))
+			return TRUE;
+	}
+	else {
+		$this->form_validation->set_message('letras', 'el campo %s tiene que tener solo letras');
+		return FALSE;
+	}
+}
+}
 ?>
