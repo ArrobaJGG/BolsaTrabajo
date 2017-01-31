91ecffe4eab698564430ffb996488416e6554a33
GorkaL <landerskate@hotmail.com>
2017-01-31 10:15:32 +0100

    top

--- assets/js/resumen_empresa.js
+++ assets/js/resumen_empresa.js
@@ -17,7 +17,10 @@ var myApp = angular.module("my-app",[]);
 					
 				);
 		
-	
+	 $scope.redirect = function($event){
+	 	console.log("hola juan");
+			$event.preventDefault();	window.location.assing = "http://www.marca.com/";
+			};
 	$scope.vermas =		function () {
 		
 		var titulo = $scope.ofertas[contador].titulo;
@@ -31,17 +34,18 @@ var myApp = angular.module("my-app",[]);
 				if (typeof titulo != 'undefined'){
 					
 						var midiv = document.createElement("div");
-						var boton = document.createElement("input");
+						var boton = document.createElement("button");
 						
 						midiv.setAttribute("id","cuadro");
-						boton.setAttribute("type" , "button");
-						boton.setAttribute("value","Editar Oferta");
-						boton.setAttribute("OnClick","location.href=OnClick=http://www.marca.com/");
 						
-						//console.log($scope.ofertas);
+						boton.setAttribute("value","Editar Oferta");
+						
+						boton.setAttribute("onclick", "window.location='./Editar_oferta_controller/index/contador'");
+						
+						
 						
 						midiv.innerHTML = "<h3>"+$scope.ofertas[contador].titulo+"</h3><p>"+$scope.ofertas[contador].resumen+"</p>";
-						boton.innerHTML= 
+						boton.innerHTML= "Editar Oferta";
 						
 						document.getElementById('cuadro').appendChild(midiv); // Lo pones en "body", si quieres ponerlo dentro de algún id en concreto usas document.getElementById('donde lo quiero poner').appendChild(midiv);
 						document.getElementById('cuadro').appendChild(boton);						
@@ -53,6 +57,10 @@ var myApp = angular.module("my-app",[]);
 				
 		
 			};
+			
+					
+			
+			
 			
 		
 
