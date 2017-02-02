8d12683e7fc88df34676c0470c3f07cb24f0ad03
GorkaG <gargoka@hotmail.com>
2017-02-02 14:55:43 +0100

    resumen alumno

--- application/views/resumen_alumno_view.php
+++ application/views/resumen_alumno_view.php
@@ -1,3 +1,16 @@
 <?php 
 $this->load->helper('form');
  ?>
+<div ng-controller="ofertaempresa">
+	<div id="tabla">
+		
+		<h1>Ofertas</h1>
+			<?php  if ($ofertas){?>
+				<div id="ofertas">
+					<h3><a href='./'</h3>
+			
+				</div>
+			<?php } else{ ?>
+		<?php } ?>
+	</div>
+</div>
