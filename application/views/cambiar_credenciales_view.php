116c7e26a2ef08d5a9c7ff58d0034b375f4c3988
JuanLuisVegaAlonso <juanlu.vega993@gmail.com>
2017-02-07 14:59:26 +0100

    Estilos cambiar credenciales

--- application/views/cambiar_credenciales_view.php
+++ application/views/cambiar_credenciales_view.php
@@ -4,11 +4,15 @@
         <input name="usuario" ng-model="usuario" ng-init="usuario='<?php echo $correo;  ?> '" required type="email"><button ng-disabled="!cambiarUsuario.$valid" type="submit">Cambiar nombre usuario</button>
     </form>-->
     <form method="post" action="<?php echo base_url('cambiar_credenciales_controller/cambiar_contrasena'); ?>" name="cambiarContrasena">
-        <span>Nueva contraseña</span>
-        <input ng-model="contrasena"required type="password" name="contrasena">
-        <br>
-        <span>Repita nueva contraseña</span>
-        <input ng-model="contrasenaRepetida" required type="password" name="contrasenaRepetida">
-        <button ng-disabled="!validarContrasenas()" type="submit">Cambiar contraseña</button>
+    	<div id="bloque">
+    		<div id="contenido">
+		        <span>Nueva contraseña</span>
+		        <input ng-model="contrasena"required type="password" name="contrasena">
+		        <br>
+		        <span>Repita nueva contraseña</span>
+		        <input ng-model="contrasenaRepetida" required type="password" name="contrasenaRepetida">
+		        <button ng-disabled="!validarContrasenas()" type="submit">Cambiar contraseña</button>
+	        </div>
+        </div>
     </form>
 </div>
--- application/views/partes/mis_ofertas.php
+++ application/views/partes/mis_ofertas.php
@@ -3,8 +3,8 @@
         <h4>Mis ofertas</h4>
     </div>
     <div class="elementoRepetible familias">
-    	<div class="elementoInterno   animate-repeat-horizontal"  ng-repeat="oferta in ofertas">
-    	    <div ng-click = "ir('/BolsaTrabajo/mostrar_ofertas_controller/index/'+oferta.id_oferta)">
+    	<div ng-click = "ir('/BolsaTrabajo/mostrar_ofertas_controller/index/'+oferta.id_oferta)" class="elementoInterno  seleccionableNav animate-repeat-horizontal"  ng-repeat="oferta in ofertas">
+    	    <div >
         		<div class="tituloOferta">{{oferta.titulo}}</div>
         		<div ng-show="oferta.nombre_empresa">Empresa: {{oferta.nombre_empresa}}</div>
         		<div>Descripcion: {{oferta.resumen}}</div>
