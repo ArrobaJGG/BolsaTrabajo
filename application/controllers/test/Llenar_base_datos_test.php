34084122ce1758d19439ae6a28b0d5e975b72809
Juan Luis Vega <juanlu.vega993@gmail.com>
2017-02-20 20:54:00 +0100

    Llenar base datos

--- application/controllers/test/Llenar_base_datos_test.php
+++ application/controllers/test/Llenar_base_datos_test.php
@@ -3,12 +3,15 @@ class Llenar_base_datos_test extends CI_Controller {
 
         public function __construct()
         {
-                parent::__construct();
-				$this->load->model('test/llenar_base_datos_model_test','usuarios');
+            parent::__construct();
+            $this->load->helper(array('form', 'url'));
+            $this->load->helper('form','url_helper');
+			$this->load->model('test/llenar_base_datos_model_test','usuarios');
         }
 		public function index(){
 			
 			$this->usuarios->usuarios();
+            redirect(base_url('/login_controller'));
 		}
 }
 
